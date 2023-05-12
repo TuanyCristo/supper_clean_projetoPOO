427da9b09435ede86e6fcfabed128c357497bfe0
Diego Mizikami <lemon7023@hotmail.com>
2023-05-11 21:34:05 -0300

    Adicionado dependencia do jCalendar

---
+++ Projeto/view/CadastroController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * 
+ */
+public class CadastroController {
+    
+    public void criarCadastro(String nome, String CPF, String email, String endereco, String senha){
+        
+
+    }
+    }
+    
